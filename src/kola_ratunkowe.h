dfcc610e6bba0bca1b7f278cd7b3cbbc5af9edf1
Hubert <hbstojek@wp.pl>
2025-04-14 17:05:33 +0200

    Update kola_ratunkowe.h
    real

--- src/kola_ratunkowe.h
+++ src/kola_ratunkowe.h
@@ -3,5 +3,7 @@
 
 typedef char string[256];
 void fiftyFifty(char correctAnswer, string answers[4]);
-
+void call_to_friend();
+void clear_input_buffer();
+void PytanieDoPublicznosci(char correctAnswer, string answers[4], string question);
 #endif
