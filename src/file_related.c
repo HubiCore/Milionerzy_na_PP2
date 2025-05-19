836b65be6223cc0a5e4395f3f03818ecd50c238d
Hubert <hbstojek@wp.pl>
2025-05-19 14:57:15 +0200

    Delete src directory

--- src/file_related.c
+++
@@ -1,7 +0,0 @@
-#include "kola_ratunkowe.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h> //losowe liczby
-#include <stdbool.h> //bool
-#include <string.h>
-#include <ctype.h>
--- src/kola_ratunkowe.h
+++
@@ -1,9 +0,0 @@
-#ifndef KOLA_RATUNKOWE_H
-#define KOLA_RATUNKOWE_H
-
-typedef char string[256];
-void fiftyFifty(char correctAnswer, string answers[4]);
-void call_to_friend();
-void clear_input_buffer();
-void PytanieDoPublicznosci(char correctAnswer, string answers[4], string question);
-#endif
